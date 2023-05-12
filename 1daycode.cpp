454dca74cdb6e927c6c8206b9cbbce9543f6680b
Siddharth Singh <singhsiddharth1102@gmail.com>
2023-05-12 13:34:39 +0530

    1st day code

---
+++ 1daycode.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+/*//int main(){
+    int a; 
+    a = 12;
+    cout<< sizeof(int)  << sizeof(a)<< endl;
+}*/
+
+int main(){
+    int amount1;
+    cin>> amount1;
+
+    int amount2;
+    cin>> amount2;
+
+    int sum = amount1+amount2;
+    cout<< sum<< endl;
+}
