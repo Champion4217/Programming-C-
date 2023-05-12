e43b134b0ab1d733cd1e0cdc9e274d16e54c6b80
Siddharth Singh <singhsiddharth1102@gmail.com>
2023-05-12 14:27:40 +0530

    complete 1 day

--- 1daycode.cpp
+++ 1daycode.cpp
@@ -6,7 +6,7 @@ using namespace std;
     cout<< sizeof(int)  << sizeof(a)<< endl;
 }*/
 
-int main(){
+/*int main(){
     int amount1;
     cin>> amount1;
 
@@ -15,4 +15,44 @@ int main(){
 
     int sum = amount1+amount2;
     cout<< sum<< endl;
+}*/
+
+
+//sum of all natural no till n
+
+/*int main(){
+    int n,s = 0;
+    for (int i =1; i<=n;i++){
+        s=s+i;
+    }
+    cout<< "sum" <<s;
+    return 0;
+}*/
+
+// print all no except which are divisible by 3 from 1- 100
+
+/*int main(){
+    for (int i =1; i<=100; i++){
+        if(i%3==0)
+        continue;
+        else
+        cout<<i;
+    }
+    return 0;
+}*/
+
+int main(){
+    int n,k=0;
+    cin>>n;
+    for ( int i=1;i<=n;i++){
+        if (n%i==0)
+        k++;
+        else
+        continue;
+
+    }
+    if(k==2)
+    cout<< "Prime no.";
+    else
+    cout<<"Not prime no.";
 }
