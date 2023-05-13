e3d0a1adf07a8f32d35bd369949498be66a86020
Siddharth Singh <singhsiddharth1102@gmail.com>
2023-05-14 00:38:23 +0530

    day 2 complete

--- 2daycode.cpp
+++ 2daycode.cpp
@@ -17,4 +17,61 @@ using namespace std;
 
     }
     return 0 ;
+}*/
+
+//----------------------Patterns------------//
+
+/*int main(){
+    int r,c,i,j;
+    cout<<"Input column"<<endl;
+    cin>>c;
+    cout<<"Input row"<<endl;
+    cin>>r;
+    for(i=1;i<=r;i++){
+        for(j=1;j<=c;j++){
+            cout<<"*" ; 
+        }
+        cout<<endl;
+    }
+    return 0;
+}*/
+
+/*int main(){
+    int r,c,i,j,g,f;
+    cout<< "rows"<<endl;
+    cin>>r;
+    cout<<"columns"<<endl;
+    cin>>c;
+    for(g=1;g<=c;g++){
+        cout<<"*";
+    }
+    cout<<endl;
+    for(i=2;i<r;i++){
+        for(j=1;j<=c;j++){
+            if(j==1||j==c)
+            cout<<"*";
+            else
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+    for(f=1;f<=c;f++){
+        cout<<"*";
+    }
+    return 0;
+}*/
+
+// -----Inverted Half Pyramid----- //
+
+int main(){
+    int i,j,rows;
+    cin>>rows;
+    for (i=1; i<=rows;i++){
+        for(j=rows;j>=i;j--){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+
 }
